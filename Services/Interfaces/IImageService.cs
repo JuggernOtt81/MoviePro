53d44e422efefeffb5694a4ce30691ab278ecf80
JuggernOtt81 <juggernott81@gmail.com>
2023-02-06 12:52:33 -0500

    create the image service interface.

--- Services/Interfaces/IImageService.cs
+++ Services/Interfaces/IImageService.cs
@@ -1,6 +1,12 @@
-﻿namespace MoviePro.Services.Interfaces
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MoviePro.Services.Interfaces
 {
     public interface IImageService
     {
+        Task<byte[]> EncodeImageAsync(IFormFile poster);
+        Task<byte[]> EncodeImageURLAsync(string imageURL);
+        string DecodeImage(byte[] poster, string contentType);
     }
 }
