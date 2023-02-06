d44abbb28db158a1e670df731481fcaadc8e4f78
JuggernOtt81 <juggernott81@gmail.com>
2023-02-06 12:41:43 -0500

    setup the RemoteMovieInterface

--- Models/ViewModels/LandingPageVM.cs
+++ Models/ViewModels/LandingPageVM.cs
@@ -1,6 +1,15 @@
-﻿namespace MoviePro.Models.ViewModels
+﻿using MoviePro.Models.DataBase;
+using MoviePro.Models.TMDB;
+using System.Collections.Generic;
+
+namespace MoviePro.Models.ViewModels
 {
     public class LandingPageVM
     {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
     }
 }
---
+++ Services/Interfaces/IRemoteMovieService.cs
@@ -0,0 +1,13 @@
+﻿using MoviePro.Models.TMDB;
+using System.Threading.Tasks;
+using MoviePro.Enums;
+
+namespace MoviePro.Services.Interfaces
+{
+    public interface IRemoteMovieService
+    {
+        Task<MovieDetail> MovieDetailAsync(int id);
+        Task<MovieSearch> SearchMoviesAsync(MovieCategory category, int count);
+        Task<ActorDetail> ActorDetailAsync(int id);
+    }
+}
