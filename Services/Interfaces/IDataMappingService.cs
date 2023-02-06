2dff66dd472bd35a21895db1b558858e4985e34a
JuggernOtt81 <juggernott81@gmail.com>
2023-02-06 12:47:38 -0500

    build the data mapping interface.

--- Services/Interfaces/IDataMappingService.cs
+++ Services/Interfaces/IDataMappingService.cs
@@ -1,6 +1,13 @@
-﻿namespace MoviePro.Services.Interfaces
+﻿using MoviePro.Models.DataBase;
+using MoviePro.Models.TMDB;
+using System.Threading.Tasks;
+
+namespace MoviePro.Services.Interfaces
 {
     public interface IDataMappingService
     {
+        Task<Movie> MapMovieDetailAsync(MovieDetail movie);
+        ActorDetail MapActorDetail(ActorDetail actor);
+        string BuildCastImage(string profilePath);
     }
 }
