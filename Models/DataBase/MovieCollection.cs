572905c537c1b3202ef84b396abc7bd429513589
JuggernOtt81 <juggernott81@gmail.com>
2023-02-06 12:16:34 -0500

    build MovieCast model

--- Models/DataBase/MovieCollection.cs
+++ Models/DataBase/MovieCollection.cs
@@ -2,5 +2,13 @@
 {
     public class MovieCollection
     {
+        public int Id { get; set; }
+        public int CollectionId { get; set; }
+        public int MovieId { get; set; }
+
+        public int Order { get; set; }
+
+        public virtual Collection Collection { get; set; }
+        public virtual Movie Movie { get; set; }
     }
 }
