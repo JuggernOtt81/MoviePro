1b9e835231bbc78f239e12a4d18ec208ac9a0a70
JuggernOtt81 <juggernott81@gmail.com>
2023-02-06 12:20:11 -0500

    create MovieCrew model

--- Models/DataBase/MovieCollection.cs
+++ Models/DataBase/MovieCollection.cs
@@ -5,9 +5,7 @@
         public int Id { get; set; }
         public int CollectionId { get; set; }
         public int MovieId { get; set; }
-
         public int Order { get; set; }
-
         public virtual Collection Collection { get; set; }
         public virtual Movie Movie { get; set; }
     }
