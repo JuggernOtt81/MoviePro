7a1c0896193be35c7894d27179268fe69cee1e9f
JuggernOtt81 <juggernott81@gmail.com>
2023-02-06 21:12:48 -0500

    Actions for the Actors Controller

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviePro.Services;
+using MoviePro.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace MoviePro.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IRemoteMovieService _tmdbMovieService;
+        private readonly IDataMappingService _tmdbMappingService;
+
+
+        public ActorsController(IRemoteMovieService tmdbMovieService, IDataMappingService tmdbMappingService)
+        {
+            _tmdbMovieService = tmdbMovieService;
+            _tmdbMappingService = tmdbMappingService;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _tmdbMappingService.MapActorDetail(actor);
+            return View(actor);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
