051c864e2bc02ef2cca07c9592018559bf4e7e0a
JuggernOtt81 <juggernott81@gmail.com>
2023-02-15 16:44:57 -0500

    backfill the seed for railway

---
+++ Helpers/DataHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MoviePro.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace MoviePro.Helpers
+{
+    public static class DataHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            //Service: An instance of db context
+            ApplicationDbContext dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            //Migration: This is the programmatic equivalent to Update-Database
+            await dbContextSvc.Database.MigrateAsync();
+        }
+    }
+}
